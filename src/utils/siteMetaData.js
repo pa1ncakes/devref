8a9720deb443fa19ecc3952bc4593596130940cc
Tarun Raj <raj.tarun@icloud.com>
2023-10-23 15:33:00 +0530

    frontent

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,19 @@
+// please update required information
+const siteMetadata = {
+  title: "devref.tech | A Reference for Developers",
+  author: "Tarun Raj",
+  headerTitle: "devref.tech",
+  description: "developer reference ",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://www.devref.tech", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "rajtarun782@gmail.com",
+  github: "https://github.com/pa1ncakes",
+  twitter: "https://twitter.com/pa1ncak3s",
+  linkedin: "https://www.linkedin.com/in/rajtarun/",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
